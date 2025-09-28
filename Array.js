02f637dd1d4d4fb27ced719f0b98a6de205a7d7d
mobjihad <mobjihad15@gmail.com>
2025-09-29 00:31:26 +0600

    Till array

--- Array.js
+++ Array.js
@@ -1,4 +1,4 @@
-// const Arr = [1,2,3,4,5];
+const Arr = [1,2,3,4,5];
 // const arr2 = ["Shaktiman", "Hola", "Deja"];
 
 // const arr3 = new Array(1,3,4,5,6,7);
@@ -14,12 +14,39 @@
 // console.log(newarr);
 // console.log(typeof newarr);
 
-const Arr1 = [1,2,3,4,5];
+// const Arr1 = [1,2,3,4,5];
 
-const arrslice = Arr1.slice(1,4);
-console.log(arrslice);
-console.log(`Main Array ; ${Arr1} `);
+// const arrslice = Arr1.slice(1,4);
+// console.log(arrslice);
+// console.log(`Main Array ; ${Arr1} `);
 
-const arrsplice = Arr1.splice(1,4);
-console.log(arrsplice);
-console.log(`Main Array ${Arr1}`);
+// const arrsplice = Arr1.splice(1,4);
+// console.log(arrsplice);
+// console.log(`Main Array ${Arr1}`);
+
+const Heros = ["Superman", "Batman","Ironman","WonderWomen", "Cat Girl"];
+const Names = ["mob","Red","RC","FIA"];
+// const newarr = Heros.concat(Names)
+// const newarr2 = [...Heros , ...Names, ...Arr] ;  
+// console.log(newarr2);
+const test = [1,2,3,4,[1,2,3,4,4,[1,2,3,4,54,[1,2,34,[1,2,3,4]]]],[1,2,3,[4,45]]];
+
+Heros.push(Names);
+Heros.push(Arr);
+Heros.push(test);
+// console.log(Heros);
+
+const anotherArray = Heros.flat(Infinity);
+// console.log(anotherArray)
+
+
+console.log(Array.isArray("MOB"));
+console.log(Array.from("MOB"));
+console.log(Array.from({name:"MOOB"}))
+
+const one= 100;
+const two= 200;
+const three= 300;
+
+const newArFM = Array.of(one,two,three)
+console.log(newArFM);
